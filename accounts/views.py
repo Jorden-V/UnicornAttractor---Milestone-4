9c46ee26a1df827f4c56ab1308590e6fbf4e5430
Ubuntu <ubuntu@ip-172-31-94-246.ec2.internal>
2019-07-23 09:19:49 +0000

    Created accounts app, started work on user login & logout functionality

---
+++ accounts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render,redirect, reverse
+from django.contrib import auth, messages
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+    
+def logout(request):
+    auth.logout(request)
+    messages.success(request, "You have successfully been logged out!")
+    return redirect (reverse('index'))
+    
+def login(request):
+    return render(request, 'login.html')
