97b4b0a3ab92befe58a2b0cf2ceb9563c5ddb37b
Ubuntu <ubuntu@ip-172-31-94-246.ec2.internal>
2019-07-23 14:28:26 +0000

    ensured users are redirected to correct pages depending on if they are logged in or not

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render,redirect, reverse
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from accounts.forms import UserLoginForm
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
-    
+
+@login_required    
 def logout(request):
     auth.logout(request)
     messages.success(request, "You have successfully been logged out!")
@@ -14,17 +16,18 @@ def logout(request):
     
 def login(request):
     """Return a login page"""
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)
 
         if login_form.is_valid():
             user = auth.authenticate(username=request.POST['username'],
                                     password=request.POST['password'])
-            
-
             if user:
                 auth.login(user=user, request=request)
                 messages.success(request, "You have successfully logged in!")
+                return redirect(reverse('index'))
             else:
                 login_form.add_error(None, "Your username or password is incorrect")
     else:
