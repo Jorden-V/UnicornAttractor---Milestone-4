c5688d4d72491a9be7c2de929ee6cc659bdf3c24
Ubuntu <ubuntu@ip-172-31-94-246.ec2.internal>
2019-07-29 11:29:30 +0000

    added cards to bugs page to display key information regarding the bug

--- bugs/urls.py
+++ bugs/urls.py
@@ -3,5 +3,5 @@ from .views import view_bugs
 
 urlpatterns = [
     url(r'^$', view_bugs, name='view_bugs'),
-    url(r'^(?P<pk>\d+)/$', view_bugs, name='view_bugs')
+    url(r'^(?P<pk>\d+)/$', view_bugs, name='view_bugs'),
     ]
