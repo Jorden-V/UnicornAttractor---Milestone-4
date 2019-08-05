becabf7a6c3994ec9c91dfc6b5e14d39dca66fc5
Ubuntu <ubuntu@ip-172-31-94-246.ec2.internal>
2019-08-05 09:41:20 +0000

    search function now in place to display features and bugs seperatley if they exist with the title of the keyword search

--- search/views.py
+++ search/views.py
@@ -4,6 +4,6 @@ from features.models import Feature
 
 # Create your views here.
 def do_search(request):
-    bugs = Bug.object.filter(name_icontains=request.GET['q'])
-    features = Feature.object.filter(name_icontains=request.GET['q'])
+    bugs = Bug.objects.filter(name__icontains=request.GET['q'])
+    features = Feature.objects.filter(name__icontains=request.GET['q'])
     return render(request, "search.html", {"bugs": bugs, "features": features})
