010e99ed69751c99413b4c754dca043d744532e8
Ubuntu <ubuntu@ip-172-31-94-246.ec2.internal>
2019-08-05 09:55:39 +0000

    updated the search view to display error messages if no key matches were made

--- search/views.py
+++ search/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from bugs.models import Bug
 from features.models import Feature
 
@@ -6,4 +7,11 @@ from features.models import Feature
 def do_search(request):
     bugs = Bug.objects.filter(name__icontains=request.GET['q'])
     features = Feature.objects.filter(name__icontains=request.GET['q'])
+    if not bugs:
+        messages.error(request, "Unfortunately your search didn't find anything")
+    elif not features:
+        messages.error(request, "Unfortunately your search didn't find anything")
+    else: 
+        bugs = bugs.order_by("-upvotes")
+        features = features.order_by("-upvotes")
     return render(request, "search.html", {"bugs": bugs, "features": features})
