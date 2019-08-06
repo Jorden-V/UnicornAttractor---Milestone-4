1616777807c7c62019c84ab65101aa6d9be3ab35
Ubuntu <ubuntu@ip-172-31-94-246.ec2.internal>
2019-08-06 14:22:29 +0000

    added and altered view from bugs to forum to allow users to view the detail of the form post where it is possible to comment on the post

--- forum/urls.py
+++ forum/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
-from .views import view_posts
+from .views import view_posts, post_detail
 
 urlpatterns = [
     url(r'^$', view_posts, name='view_posts'),
+    url(r'^(?P<pk>\d+)/$', post_detail, name='post_detail'),
     ]
--- forum/views.py
+++ forum/views.py
@@ -5,3 +5,30 @@ from .forms import ForumPostForm, ForumCommentForm
 def view_posts(request):
     posts = ForumPost.objects.all()
     return render(request, "forum.html", {"posts": posts})
+    
+def post_detail(request, pk):
+    """
+    Create a view that returns a single
+    bug object based on the bug ID (pk) and
+    render it to the bug_detail.html template
+    or return 404 error if object is not found
+    """
+    post = get_object_or_404(ForumPost, pk=pk)
+    if request.method == "POST":
+        
+        form = ForumCommentForm(request.POST)
+        
+        if form.is_valid():
+            postComment = form.save(commit=False)
+            postComment.post = post
+            postComment.author = request.user
+            postComment.save()
+            return redirect(reverse('post_detail', kwargs={'pk': pk}))
+            
+    else:
+        form = ForumCommentForm()
+        comments = ForumComment.objects.filter(post__pk=post.pk)
+        comments_total = len(comments)
+        post.views += 1
+        post.save()
+        return render(request, 'post_detail.html', {'post':post, 'comments':comments, 'comments_total':comments_total, 'form':form})
