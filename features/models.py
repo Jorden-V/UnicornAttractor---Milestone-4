d47cef5020709bc7e3ae42e4b7b5a538d6670474
Ubuntu <ubuntu@ip-172-31-94-246.ec2.internal>
2019-08-06 12:00:03 +0000

    updated features model to also include status. Migrated new models

--- features/models.py
+++ features/models.py
@@ -11,6 +11,18 @@ class Feature(models.Model):
     upvotes = models.IntegerField(default=0)
     views = models.IntegerField(default=0)
     author = models.ForeignKey(User)
+    
+    STATUS_CHOICES = (
+        ('To do', 'To do'),
+        ('In progress', 'In progress'),
+        ('Done', 'Done'),
+        ('Cancelled', 'Cancelled')
+    )
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='To do'
+    )
 
     def __str__(self):
         return self.name
