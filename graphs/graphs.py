a6c95659d26b204d06ee3a4f40e4c55e6f9de2cc
Ubuntu <ubuntu@ip-172-31-94-246.ec2.internal>
2019-08-06 13:25:14 +0000

    replicated bugs pie chart for features

--- graphs/graphs.py
+++ graphs/graphs.py
@@ -87,3 +87,28 @@ def BugPieChart():
     chart = bug_pie_chart()
     return chart
     
+def feature_pie_chart():
+    
+    """A pie chart showing the status of features"""
+
+    todo = Feature.objects.filter(status='To do').count()
+    inprogress = Feature.objects.filter(status='In progress').count()
+    done = Feature.objects.filter(status='Done').count()
+    cancelled = Feature.objects.filter(status='Cancelled').count()
+    p_chart = pygal.Pie(
+        print_values=True,
+        style=custom_style
+    )
+
+    p_chart.add('To Do', todo)
+    p_chart.add('In Progress', inprogress)
+    p_chart.add('Done', done)
+    p_chart.add('Cancelled', cancelled)
+    return p_chart.render()
+
+def FeaturePieChart():
+    
+    """Enable importing the graph into a view"""
+    
+    chart = feature_pie_chart()
+    return chart 
