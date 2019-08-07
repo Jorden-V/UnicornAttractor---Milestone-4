98f70226689d01a5e9ca6cbd6200c163de39217f
Ubuntu <ubuntu@ip-172-31-94-246.ec2.internal>
2019-08-07 13:53:48 +0000

    added a test confirming the bug model description is set to false

---
+++ bugs/test_forms.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+from .forms import CreateBugForm, BugCommentForm
+
+# Create your tests here.
+class TestBugForm(TestCase):
+    
+    def test_can_create_a_bug_with_name_and_description(self):
+        form = CreateBugForm({'name': 'Tests', 'desc': "create a test"})
+        self.assertTrue(form.is_valid())
+        
+    def test_cannot_create_a_bug_without_required_values(self):
+        form = CreateBugForm({'name': 'Test'})
+        self.assertFalse(form.is_valid())
+    
+    def test_correct_message_for_missing_name(self):
+        form = CreateBugForm({'form': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['name'], [u'This field is required.'])
+        
+    def test_correct_message_for_missing_desc(self):
+        form = CreateBugForm({'form': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['desc'], [u'This field is required.'])
+        
+class TestBugCommentForm(TestCase):
+    
+    def test_can_create_a_comment_with_required_values(self):
+        form = BugCommentForm({'description': "comment"})
+        self.assertTrue(form.is_valid())
+        
+    def test_cannot_post_blank_comment(self):
+        form = BugCommentForm({'description': ''})
+        self.assertFalse(form.is_valid())
--- bugs/tests_models.py
+++ bugs/tests_models.py
@@ -1,33 +1,18 @@
-from django.test import TestCase
-from .forms import CreateBugForm, BugCommentForm
+from django.test import TestCase, Client
+from django.contrib import auth
+from django.contrib.auth.models import User
+from .models import Bug, BugComment
 
-# Create your tests here.
-class TestBugForm(TestCase):
+class TestBugModel(TestCase):
     
-    def test_can_create_a_bug_with_name_and_description(self):
-        form = CreateBugForm({'name': 'Tests', 'desc': "create a test"})
-        self.assertTrue(form.is_valid())
-        
-    def test_cannot_create_a_bug_without_required_values(self):
-        form = CreateBugForm({'name': 'Test'})
-        self.assertFalse(form.is_valid())
-    
-    def test_correct_message_for_missing_name(self):
-        form = CreateBugForm({'form': ''})
-        self.assertFalse(form.is_valid())
-        self.assertEqual(form.errors['name'], [u'This field is required.'])
-        
-    def test_correct_message_for_missing_desc(self):
-        form = CreateBugForm({'form': ''})
-        self.assertFalse(form.is_valid())
-        self.assertEqual(form.errors['desc'], [u'This field is required.'])
-        
-class TestBugCommentForm(TestCase):
-    
-    def test_can_create_a_comment_with_required_values(self):
-        form = BugCommentForm({'description': "comment"})
-        self.assertTrue(form.is_valid())
-        
-    def test_cannot_post_blank_comment(self):
-        form = BugCommentForm({'description': ''})
-        self.assertFalse(form.is_valid())
+    def setUp(self):
+        user = User.objects.create_user(username="test", password="testing")
+        self.client.login(username="test", password="testing")
+        bug = Bug.objects.create(name="test", desc="testing", author_id=user.id)
+
+    def test_desc_defaults_to_False(self):
+        user = User.objects.get(username="test")
+        bug = Bug(name="Create a Test", author_id=user.id)
+        bug.save()
+        self.assertEqual(bug.name, "Create a Test")
+        self.assertFalse(bug.desc)
