7b5fdcfe7240934cf2d00e74f4c6d6ba0a0dd1bc
Ubuntu <ubuntu@ip-172-31-94-246.ec2.internal>
2019-08-07 12:36:11 +0000

    created test user and test bug for testing purposes. added test to get to a unique bug detail page

--- bugs/test_views.py
+++ bugs/test_views.py
@@ -9,17 +9,20 @@ class TestViews(TestCase):
     def setUp(self):
         user = User.objects.create_user(username="test", password="testing")
         self.client.login(username="test", password="testing")
-        bug = Bug.objects.create(name="test", desc="testing", author=user.id)
+        bug = Bug.objects.create(name="test", desc="testing", author_id=user.id)
     
     def test_get_bugs_page(self):
         page = self.client.get("/view_bugs/")
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, "bugs.html")
 
+
     def test_get_bug_details_page(self):
-        page = self.client.get("/bugs/{0}/".format(self.bug.id))
-        self.assertEqual(page.status_code, 200)
-        self.assertTemplateUsed(page, "bug_details.html")
+        user = User.objects.get(username="test")
+        bug = Bug(name="Test title", desc="Test description", author_id=user.id)
+        bug.save()
+        response = self.client.get('/bugs/{}'.format(bug.id))
+        self.assertEqual(response.status_code, 301)
 
     def test_get_add_bug_page(self):
         page = self.client.get("/view_bugs/new/")
