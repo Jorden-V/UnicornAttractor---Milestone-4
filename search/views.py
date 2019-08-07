9ed3fc39670225fd774ca5a5f96f733eee0097d0
Ubuntu <ubuntu@ip-172-31-94-246.ec2.internal>
2019-08-07 09:22:54 +0000

    updated search view to find forum posts as well

--- search/views.py
+++ search/views.py
@@ -2,16 +2,21 @@ from django.shortcuts import render
 from django.contrib import messages
 from bugs.models import Bug
 from features.models import Feature
+from forum.models import ForumPost
 
 # Create your views here.
 def do_search(request):
     bugs = Bug.objects.filter(name__icontains=request.GET['q'])
     features = Feature.objects.filter(name__icontains=request.GET['q'])
+    posts = ForumPost.objects.filter(name__icontains=request.GET['q'])
     if not bugs:
-        messages.error(request, "Unfortunately your search didn't find anything")
+        messages.error(request, "Unfortunately your search didn't find any bugs")
     elif not features:
-        messages.error(request, "Unfortunately your search didn't find anything")
+        messages.error(request, "Unfortunately your search didn't find any features")
+    elif not posts:
+        messages.error(request, "Unfortunately your search didn't find any posts")
     else: 
         bugs = bugs.order_by("-upvotes")
         features = features.order_by("-upvotes")
-    return render(request, "search.html", {"bugs": bugs, "features": features})
+        posts = posts.order_by("-upvotes")
+    return render(request, "search.html", {"bugs": bugs, "features": features, "posts": posts})
