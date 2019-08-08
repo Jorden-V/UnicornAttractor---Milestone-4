ab3a4331e57aeea2dc29c25e800408667b7c8291
Ubuntu <ubuntu@ip-172-31-94-246.ec2.internal>
2019-08-08 09:30:18 +0000

    replicated bug tests for features - test forms

---
+++ features/test_forms.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+from .forms import CreateFeatureForm, FeatureCommentForm
+
+# Create your tests here.
+class TestFeatureForm(TestCase):
+    
+    def test_can_create_a_feature_with_name_and_description(self):
+        form = CreateFeatureForm({'name': 'Tests', 'desc': "create a test"})
+        self.assertTrue(form.is_valid())
+        
+    def test_cannot_create_a_feature_without_required_values(self):
+        form = CreateFeatureForm({'name': 'Test'})
+        self.assertFalse(form.is_valid())
+    
+    def test_correct_message_for_missing_name(self):
+        form = CreateFeatureForm({'form': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['name'], [u'This field is required.'])
+        
+    def test_correct_message_for_missing_desc(self):
+        form = CreateFeatureForm({'form': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['desc'], [u'This field is required.'])
+        
+class TestFeatureCommentForm(TestCase):
+    
+    def test_can_create_a_comment_with_required_values(self):
+        form = FeatureCommentForm({'description': "comment"})
+        self.assertTrue(form.is_valid())
+        
+    def test_cannot_post_blank_comment(self):
+        form = FeatureCommentForm({'description': ''})
+        self.assertFalse(form.is_valid())
