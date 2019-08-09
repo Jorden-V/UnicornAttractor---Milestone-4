037eec6acaf9f14f1c81928e38a283afa89df381
Ubuntu <ubuntu@ip-172-31-94-246.ec2.internal>
2019-08-09 15:03:13 +0000

    redid the search html page to reflect similar to the profile html page. seperated the posts, features and bugs but also added a all search result tab

--- search/views.py
+++ search/views.py
@@ -11,14 +11,7 @@ def do_search(request):
     bugs = Bug.objects.filter(name__icontains=request.GET['q'])
     features = Feature.objects.filter(name__icontains=request.GET['q'])
     posts = ForumPost.objects.filter(name__icontains=request.GET['q'])
-    if not bugs:
-        messages.error(request, "Unfortunately your search didn't find any bugs")
-    elif not features:
-        messages.error(request, "Unfortunately your search didn't find any features")
-    elif not posts:
-        messages.error(request, "Unfortunately your search didn't find any posts")
-    else: 
-        bugs = bugs.order_by("-upvotes")
-        features = features.order_by("-upvotes")
-        posts = posts.order_by("-upvotes")
+    bugs = bugs.order_by("-upvotes")
+    features = features.order_by("-upvotes")
+    posts = posts.order_by("-upvotes")
     return render(request, "search.html", {"bugs": bugs, "features": features, "posts": posts})
