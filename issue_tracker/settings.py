e6e53cef51927913ce1cde8437325ce2eb9031e2
Ubuntu <ubuntu@ip-172-31-94-246.ec2.internal>
2019-08-13 15:54:24 +0000

    added .pyc files to gitignore

--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -149,3 +149,4 @@ EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
