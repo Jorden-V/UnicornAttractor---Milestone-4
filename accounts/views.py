1a8e51dd7fcf92b9c6e660297c34381d013690e3
Ubuntu <ubuntu@ip-172-31-94-246.ec2.internal>
2019-08-19 09:54:32 +0000

    fixed type in registeration view to style success message

--- accounts/views.py
+++ accounts/views.py
@@ -74,10 +74,10 @@ def registration(request):
                                      password=request.POST['password1'])
             if user:
                 auth.login(user=user, request=request)
-                messages.success(request, "You have successfully registered", extra_tags="altert-success")
+                messages.success(request, "You have successfully registered", extra_tags="alert-success")
                 return redirect(reverse('index'))
             else:
-                messages.error(request, "Unable to register your account at this time", extra_tags="altert-danger")
+                messages.error(request, "Unable to register your account at this time", extra_tags="alert-danger")
     else:
         registration_form = UserRegistrationForm()
     return render(request, 'registration.html', {
