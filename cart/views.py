d0a558c61f8f398ed3c1e145f06677283c3631fc
Ubuntu <ubuntu@ip-172-31-94-246.ec2.internal>
2019-08-22 10:45:19 +0000

    validated python files in cart app

--- cart/views.py
+++ cart/views.py
@@ -6,21 +6,28 @@ from django.contrib import messages
 @login_required()
 def view_cart(request):
     """A View that renders the cart contents page"""
-    return render(request, "cart.html" )
+    return render(request, "cart.html")
+
 
 @login_required()
-
 def add_to_cart(request, id):
     """Add a quantity of the specified product to the cart"""
     quantity = 1
     cart = request.session.get('cart', {})
     if id in cart.keys():
-        messages.error(request, "This feature is already in your cart!", extra_tags="alert-danger")
+        messages.error(
+            request,
+            "This feature is already in your cart!",
+            extra_tags="alert-danger")
     else:
         cart[id] = cart.get(id, quantity)
         request.session['cart'] = cart
-        messages.success(request, "Feature successfully added to cart", extra_tags="alert-success")
+        messages.success(
+            request,
+            "Feature successfully added to cart",
+            extra_tags="alert-success")
     return redirect('view_features')
+
 
 @login_required()
 def adjust_cart(request, id):
