e03dde2cb3c7338b766a3e18ae4774e557a72f6d
Ubuntu <ubuntu@ip-172-31-94-246.ec2.internal>
2019-09-09 13:19:35 +0000

    fixed submit button so it correctly disables once user completed payment. Also changes message

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,13 @@
-/* ---------- Disables payment button once clicked and submits form ---------*/
-$(function() {
-    $('#submit_payment_btn').on('click', function() {
-         $("#submit_payment_btn").attr("disabled", true);
-         $("#payment-form").submit();
-    });
-});
-
-/* ---------- Resets payment form and re-enables payment button ---------*/
-$('#payment-reset').click(function() {
-  $('#form-reset').trigger("reset");
-  $('#submit_payment_btn').prop('disabled', false);
-});
-
 /* ---------- Loads modal to display information on payment ---------*/
 $(window).on('load', function() {
     $('#paymentGuide').modal('show');
+  });
+
+
+/* ---------- changes text when payment form is submitted ---------*/
+$('#payment-form').submit(function()
+ {
+    $("input[type='submit']", this)
+      .val("Please Wait...");
+    return true;
   });
--- static/js/stripe.js
+++ static/js/stripe.js
@@ -30,3 +30,5 @@ $(function() {
     return false;
     });
 });
+
+
